a991e0fbe540a47c4ccee09002def80eae5f5eba
LiangLi17 <3170104962@zju.edu.cn>
2021-03-26 19:14:19 +0800

    complete lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null){
+            return B;
+        }
+        if (B == null){
+            return A;
+        }
         IntList ptr = A;
         while (A.rest != null){
             A = A.rest;
